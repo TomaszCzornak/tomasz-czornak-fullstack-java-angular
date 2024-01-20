75f0e57c3f0786112b95b33f1d7f928211a9c237
TomaszCzornak <tomasz.czornak@outlook.com>
2024-01-20 17:36:37 +0100

    kod startowy do springa

---
+++ reskilled/src/test/java/reskilled/mentoring/reskilled/ReskilledApplicationTests.java
@@ -0,0 +1,13 @@
+package reskilled.mentoring.reskilled;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReskilledApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
