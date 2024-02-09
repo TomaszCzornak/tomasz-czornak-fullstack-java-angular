a18f5ec1b6096faaf095917923188495cff87b70
TomaszCzornak <tomasz.czornak@outlook.com>
2024-02-09 11:55:52 +0100

    Migracja danych do bazy H2 poprawione typ ID w job_skill

--- reskilled/src/main/resources/schema.sql
+++ reskilled/src/main/resources/schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE Skill
 
 CREATE TABLE job_skill
 (
-    job_id   CHAR(36) NOT NULL,
+    job_id   INT NOT NULL,
     skill_id INT      NOT NULL,
     PRIMARY KEY (job_id, skill_id),
     FOREIGN KEY (job_id) REFERENCES Job (id),
