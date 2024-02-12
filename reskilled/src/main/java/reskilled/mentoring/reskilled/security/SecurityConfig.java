15e682bc765eaf68f02b0059956973f286f04e68
TomaszCzornak <tomasz.czornak@outlook.com>
2024-02-12 16:24:58 +0100

    Rejestracja - poprawione security

--- reskilled/src/main/java/reskilled/mentoring/reskilled/security/SecurityConfig.java
+++ reskilled/src/main/java/reskilled/mentoring/reskilled/security/SecurityConfig.java
@@ -4,9 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -14,16 +13,10 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-       return http
-                .authorizeHttpRequests()
-                .requestMatchers("/h2-console/**").permitAll()
-                .anyRequest().fullyAuthenticated()
-                .and()
-                .formLogin(withDefaults())
-                .csrf().ignoringRequestMatchers("/h2-console/**")
-                .and()
-                .headers().frameOptions().sameOrigin()
-                .and()
-                .build();
+        http.authorizeHttpRequests((requests) -> requests.requestMatchers("/", "/v1/register").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin((form) -> form.loginPage("/login").permitAll())
+                .logout(LogoutConfigurer::permitAll);
+        return http.build();
     }
-    }
+}
