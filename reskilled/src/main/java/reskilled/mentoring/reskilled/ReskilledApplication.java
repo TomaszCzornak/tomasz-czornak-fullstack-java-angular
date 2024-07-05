8ad25f43d76e3bb176a278594d1056720b7c2871
TomaszCzornak <tomasz.czornak@outlook.com>
2024-07-05 19:17:11 +0200

    10. Seeds - data generating methods to seed data

--- reskilled/src/main/java/reskilled/mentoring/reskilled/ReskilledApplication.java
+++ reskilled/src/main/java/reskilled/mentoring/reskilled/ReskilledApplication.java
@@ -1,15 +1,29 @@
 package reskilled.mentoring.reskilled;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reskilled.mentoring.reskilled.infrastructure.Seeds;
 
 @SpringBootApplication
-public class ReskilledApplication {
+public class ReskilledApplication implements CommandLineRunner {
 
+    private final Seeds seeds;
 
+    public ReskilledApplication(Seeds seeds) {
+        this.seeds = seeds;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ReskilledApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        seeds.generateUsers();
+        seeds.generateSkills();
+        seeds.generateJobs();
+        seeds.generateCandidates();
+        seeds.generateRecruitments();
+    }
 }
